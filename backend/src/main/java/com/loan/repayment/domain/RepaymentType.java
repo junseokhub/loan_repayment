5830fda41ec82c501e4007df37f76ab5402acf87
junseokhub <github.yam117@passinbox.com>
2026-04-16 00:15:29 +0900

    Fix: RepaymentType Enum naming

--- backend/src/main/java/com/loan/repayment/domain/RepaymentType.java
+++ backend/src/main/java/com/loan/repayment/domain/RepaymentType.java
@@ -1,9 +1,9 @@
 package com.loan.repayment.domain;
 
 public enum RepaymentType {
-    EQUAL_INSTALLMENT("원리금균등상환"),
-    BULLET("만기일시상환"),
-    EQUAL_PRINCIPAL("원금균등상환");
+    EQUAL_INSTALLMENT("Equal Installment Repayment"),
+    BULLET("Bullet Repayment"),
+    EQUAL_PRINCIPAL("Equal Principal Repayment");
 
     private final String type;
 
